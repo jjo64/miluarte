bd58548d0faa967cf9743d48abfaaab2eca8e882
jo <cuevajosuelm@gmail.com>
2026-06-18 22:59:42 +0200

    feat: integrar ThemeProvider y LanguageProvider en el componente raíz App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,15 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { ThemeProvider } from "./context/ThemeContext";
+import { LanguageProvider } from "./context/LanguageContext";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <RouterProvider router={router} />
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
+
